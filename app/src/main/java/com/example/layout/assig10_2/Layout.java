7ddb99630a8c938abc2aa2c8353f449f19dfd1b4
saipriyanka96 <priyanaru96@gmail.com>
2018-01-23 10:29:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/layout/assig10_2/Layout.java
@@ -0,0 +1,25 @@
+package com.example.layout.assig10_2;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class Layout extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //creates and returns the hierarchy associated with the fragment
+        //the layout inflater object can be used to inflate any views in the fragment
+        //the view group contains views and is base class of layouts and views containers
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_layout, container, false);
+        //returns the inflater with respective layout
+    }
+
+}
---
+++ app/src/main/java/com/example/layout/assig10_2/OnitemSelectedListener.java
@@ -0,0 +1,11 @@
+package com.example.layout.assig10_2;
+
+/**
+ * Created by User on 03-01-2018.
+ */
+
+ interface OnitemSelectedListener {
+     //creating an interface for an item listener
+    public void onTrpItemSelected(String link);
+
+}
